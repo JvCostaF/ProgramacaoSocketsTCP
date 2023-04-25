e73a8584ff80d6d9d221d9e4b5e2c0dac1f7cdff
João Víctor da Costa <joaovcosta220@gmail.com>
2023-04-25 19:20:09 -0300

    Implementa o TCPServer
    Seguindo o exemplo do livro para testar.

--- src/TCPServer.java
+++ src/TCPServer.java
@@ -1,2 +1,28 @@
+import java.io.*;
+import java.net.*;
 public class TCPServer {
+
+    public static void main(String[] args) throws IOException {
+
+        String clientSentence;
+        String capitalizedSentence;
+
+        ServerSocket serverSocket = new ServerSocket(4000);
+
+        while (true) {
+            Socket connectionSocket = serverSocket.accept();
+
+            System.out.println("Servidor ouvindo!");
+
+            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+
+            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+
+            clientSentence = inFromClient.readLine();
+
+            capitalizedSentence = clientSentence.toUpperCase() + '\n';
+
+            outToClient.writeBytes(capitalizedSentence);
+        }
+    }
 }
