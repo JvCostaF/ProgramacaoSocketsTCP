eac3e0335ef63090cb37980b089daadcffedc194
João Víctor da Costa <joaovcosta220@gmail.com>
2023-04-25 19:20:29 -0300

    Implementa o TCPClient
    Seguindo o exemplo do livro para testar.

--- src/TCPClient.java
+++ src/TCPClient.java
@@ -1,2 +1,28 @@
+import java.io.*;
+import java.net.*;
+
 public class TCPClient {
+    public static void main(String[] args) throws IOException {
+
+        String sentence;
+        String modifiedSentece;
+
+        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+
+        Socket clientSocket = new Socket("192.168.0.20",4000);
+
+        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+
+        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+        sentence = inFromUser.readLine();
+
+        outToServer.writeBytes(sentence + '\n');
+
+        modifiedSentece = inFromServer.readLine();
+
+        System.out.println("From Server: " + modifiedSentece);
+
+        clientSocket.close();
+    }
 }
